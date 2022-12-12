e8e955685dcc3d299c383affc1969f7c2d19ecab
Prakhar Porwal <prakhar@bug.video>
2022-12-12 13:35:38 +0530

    #90 - post-build-plugin.sh added | JNA and not able to install on linux,

---
+++ post-build-plugin.sh
@@ -0,0 +1,17 @@
+!/bin/bash
+
+export PLUGIN_VERSION=1.8.23
+
+cd build/distributions
+unzip plugin-${PLUGIN_VERSION}.zip
+
+cd plugin/lib
+rm jna-5.5.0.jar jna-platform-5.5.0.jar
+
+cd ../..
+
+zip -r -Z deflate plugin-${PLUGIN_VERSION}-nc.zip plugin/*
+
+rm -rf plugin
+
+cd ../..
