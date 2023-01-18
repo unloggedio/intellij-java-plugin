efd77e6a4188ff6c1c03ae389f112df048283bd2
Prakhar Porwal <prakhar@bug.video>
2023-01-18 15:05:31 +0530

    Optimised Enum resolving Call

--- src/main/java/com/insidious/plugin/client/SessionInstance.java
+++ src/main/java/com/insidious/plugin/client/SessionInstance.java
@@ -38,6 +38,7 @@ import com.insidious.plugin.pojo.*;
 import com.insidious.plugin.pojo.dao.*;
 import com.insidious.plugin.ui.NewTestCandidateIdentifiedListener;
 import com.insidious.plugin.util.LoggerUtil;
+import com.insidious.plugin.util.StringUtils;
 import com.intellij.lang.jvm.JvmMethod;
 import com.intellij.lang.jvm.JvmParameter;
 import com.intellij.lang.jvm.types.JvmType;
@@ -3480,7 +3481,7 @@ public class SessionInstance {
         if (loadCalls) {
             for (MethodCallExpression methodCallExpression : testCandidateMetadata.getCallsList()) {
                 resolveTemplatesInCall(methodCallExpression);
-//                createParamEnumPropertyTrueIfTheyAre(methodCallExpression);
+                createParamEnumPropertyTrueIfTheyAre(methodCallExpression);
             }
         }
         return testCandidateMetadata;
@@ -3503,25 +3504,18 @@ public class SessionInstance {
         if (param == null || param.getType() == null)
             return;
 
-        // todo : Optimise this enum type search in classInfoIndex
-        for (ChronicleMap.Entry<Integer, ClassInfo> entry : this.classInfoIndex.entrySet()) {
-            String currParamType = param.getType()
-                    .replace('.', '/');
-            ClassInfo currClassInfo = entry.getValue();
-            if (currClassInfo.getClassName()
-                    .equals(currParamType)) {
-                // curr class info is present and is enum set param as enum
-                if (currClassInfo.isEnum()) {
-                    param.setIsEnum(true);
+        String currParamType = param.getType();
+        ClassInfo currClass = this.classInfoIndexByName.get(currParamType);
+        
+        if (currClass != null && currClass.isEnum()) {
+            param.setIsEnum(true);
 
-                    //change Name Of Param to use a camelCase and lowercase
-//                    List<String> names = param.getNamesList();
-//                    if (names != null && names.size() > 0) {
-//                        String modifiedName = StringUtils.convertSnakeCaseToCamelCase(names.get(0));
-//                        names.remove(0);
-//                        names.add(0, modifiedName);
-//                    }
-                }
+            // curr param name converted to camelCase
+            List<String> names = param.getNamesList();
+            if (names != null && names.size() > 0) {
+                String modifiedName = StringUtils.convertSnakeCaseToCamelCase(names.get(0));
+                names.remove(0);
+                names.add(0, modifiedName);
             }
         }
     }
