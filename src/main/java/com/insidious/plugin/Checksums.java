6b6c3f3246e668ca0711b0b07d7efdb3ae9c0e1a
artpar <artpar@gmail.com>
2023-01-27 13:04:13 +0530

    add jackson 2.8 md5 and switch

--- src/main/java/com/insidious/plugin/Checksums.java
+++ src/main/java/com/insidious/plugin/Checksums.java
@@ -3,7 +3,6 @@ package com.insidious.plugin;
 public class Checksums {
 
     public static String AGENT_GSON = "ebf68607e6b21b4c783b9d7a1bddadaf";
-
     public static String AGENT_JACKSON_2_8 = "fa41ddd90f00e312e5eb9c856948e35a";
     public static String AGENT_JACKSON_2_9 = "dca8ad2a34c403439f5ceac32590b8c1";
     public static String AGENT_JACKSON_2_10 = "4994fc4e8ea1a5744eb5c22544100a76";
