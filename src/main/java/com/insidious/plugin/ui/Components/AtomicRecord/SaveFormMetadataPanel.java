e689f9037dcdce5f41a9e6fd4b16dbf3b1f84fac
Amogh CR <68552461+TheAmg@users.noreply.github.com>
2023-07-21 18:40:37 +0530

    icon addition to status in metadataview, margin addition to textArea

--- src/main/java/com/insidious/plugin/ui/Components/AtomicRecord/SaveFormMetadataPanel.java
+++ src/main/java/com/insidious/plugin/ui/Components/AtomicRecord/SaveFormMetadataPanel.java
@@ -1,6 +1,7 @@
 package com.insidious.plugin.ui.Components.AtomicRecord;
 
 import com.insidious.plugin.pojo.atomic.StoredCandidateMetadata;
+import com.insidious.plugin.util.UIUtils;
 
 import javax.swing.*;
 import java.time.Instant;
@@ -36,7 +37,19 @@ public class SaveFormMetadataPanel {
         this.hostLabel.setText(payload.getStoredCandidateMetadata().getHostMachineName());
         this.timestampLabel.setText(convertTimestamp(payload.getStoredCandidateMetadata().getTimestamp()));
         this.timestampLabel.setToolTipText(payload.getStoredCandidateMetadata().getTimestamp()+"");
-        this.statusLabel.setText(payload.getStoredCandidateMetadata().getCandidateStatus().toString());
+        StoredCandidateMetadata.CandidateStatus status = payload.getStoredCandidateMetadata().getCandidateStatus();
+        if(status.equals(StoredCandidateMetadata.CandidateStatus.FAILING))
+        {
+            this.statusLabel.setIcon(UIUtils.DIFF_GUTTER);
+            this.statusLabel.setForeground(UIUtils.red);
+            this.statusLabel.setText("Failing");
+        }
+        else
+        {
+            this.statusLabel.setIcon(UIUtils.NO_DIFF_GUTTER);
+            this.statusLabel.setForeground(UIUtils.green);
+            this.statusLabel.setText("Passing");
+        }
     }
 
     public MetadataViewPayload getPayload()
