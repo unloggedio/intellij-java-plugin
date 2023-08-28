7caf65b9f09057f7e9212027df784e963ffdbf68
artpar <artpar@gmail.com>
2023-08-28 17:56:30 +0530

    Unlogged sdk version to 0.0.24

--- src/main/java/com/insidious/plugin/ui/GutterClickNavigationStates/UnloggedSDKOnboarding.java
+++ src/main/java/com/insidious/plugin/ui/GutterClickNavigationStates/UnloggedSDKOnboarding.java
@@ -13,7 +13,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 
 public class UnloggedSDKOnboarding {
-    private final String UNLOGGED_SDK_VERSION = "0.0.23";
+    private final String UNLOGGED_SDK_VERSION = "0.0.24";
     private JPanel mainPanel;
     private JPanel topAligner;
     private JPanel infoPanel;
