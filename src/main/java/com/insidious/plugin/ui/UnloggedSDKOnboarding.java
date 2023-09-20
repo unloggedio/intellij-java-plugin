85665616fabf044b7577440aa16e332543061645
artpar <artpar@gmail.com>
2023-09-20 16:54:20 +0530

    sdk version 0.1.8

--- src/main/java/com/insidious/plugin/ui/UnloggedSDKOnboarding.java
+++ src/main/java/com/insidious/plugin/ui/UnloggedSDKOnboarding.java
@@ -13,7 +13,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 
 public class UnloggedSDKOnboarding {
-    private final String UNLOGGED_SDK_VERSION = "0.1.7";
+    private final String UNLOGGED_SDK_VERSION = "0.1.8";
     private JPanel mainPanel;
     private JPanel topAligner;
     private JPanel infoPanel;
