160f404f0ce9daaacd3c15987d54c0f36f1b5604
artpar <artpar@gmail.com>
2024-04-10 18:52:59 +0530

    0.4.1

--- src/main/java/com/insidious/plugin/Constants.java
+++ src/main/java/com/insidious/plugin/Constants.java
@@ -19,6 +19,6 @@ public class Constants {
             .getPath(System.getProperty("user.home"), ".unlogged");
     public static final String COMPLETED = "completed";
     public static final String PENDING = "pending";
-    public static final String AGENT_VERSION = "0.3.10";
+    public static final String AGENT_VERSION = "0.4.1";
     public static final String HOSTNAME = getHostname();
 }
