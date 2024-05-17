beeb1674eacc88144a132f7003ef38952f36bdd7
Kartikey Tewari <kt@unlogged.io>
2024-05-17 20:30:02 +0530

    remove sessionDiscovery from SessionLoader

--- src/main/java/com/insidious/plugin/factory/SessionLoader.java
+++ src/main/java/com/insidious/plugin/factory/SessionLoader.java
@@ -32,7 +32,7 @@ public class SessionLoader implements Runnable, GetProjectSessionsCallback, Disp
     private final ExecutorService ourPool;
     private final List<GetProjectSessionsCallback> listeners = new ArrayList<>();
     private VideobugClientInterface client;
-    private List<ExecutionSession> lastResult;
+    private List<ExecutionSession> lastResult = new ArrayList<>();
 
     public SessionLoader() {
         ourPool = Executors.newFixedThreadPool(1, new DefaultThreadFactory("UnloggedAppThreadPool"));
@@ -90,87 +90,4 @@ public class SessionLoader implements Runnable, GetProjectSessionsCallback, Disp
     public void dispose() {
         ourPool.shutdown();
     }
-
-	private void get(String url, Callback callback) {
-		
-		final OkHttpClient httpClient = new OkHttpClient().newBuilder()
-                .connectTimeout(600, TimeUnit.SECONDS)
-                .readTimeout(600, TimeUnit.SECONDS)
-                .writeTimeout(600, TimeUnit.SECONDS)
-                .build();
-        
-		Request.Builder builder = new Request.Builder().url(url);
-        Request request = builder.build();
-        Call call = httpClient.newCall(request);
-        call.enqueue(callback);
-
-        if (ProgressIndicatorProvider.getGlobalProgressIndicator() != null) {
-            String dots = "";
-            while (true) {
-                try {
-                    Thread.sleep(500);
-                    if (call.isExecuted()) {
-                        break;
-                    }
-
-                    dots = dots + ".";
-                    if (dots.length() > 3) {
-                        dots = ".";
-                    }
-                    ProgressIndicatorProvider.getGlobalProgressIndicator()
-                            .setText2("Query is in progress " + dots);
-                    if (ProgressIndicatorProvider.getGlobalProgressIndicator()
-                            .isCanceled()) {
-                        throw new ProcessCanceledException();
-                    }
-
-                } catch (InterruptedException e) {
-                    throw new ProcessCanceledException(e);
-                }
-            }
-        }
-
-    }
-
-	public List<ExecutionSession> sessionDiscovery(SourceModel sourceModel, String packageName){
-		
-		List<ExecutionSession> executionSessionList = new ArrayList<>();
-		if (sourceModel.getServerEndpoint() == "") {
-			return executionSessionList;
-		}
-
-		String url = sourceModel.getServerEndpoint() + "/discovery" + "?packageName=" + packageName;
-        CountDownLatch latch = new CountDownLatch(1);
-		get(url, new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-                logger.info("failure encountered");
-                latch.countDown();
-            }
-
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                try {
-					ObjectMapper objectMapper = new ObjectMapper();
-					String responseBody = Objects.requireNonNull(response.body()).string();
-					ExecutionSession[] executionSessionLocal = objectMapper.readValue(responseBody, ExecutionSession[].class);
-					for (int i=0;i<=executionSessionLocal.length-1;i++) {
-						executionSessionLocal[i].setSessionMode(SessionMode.REMOTE);
-						executionSessionList.add(executionSessionLocal[i]);
-					}
-                } finally {
-                    response.close();
-                    latch.countDown();
-                }
-            }
-        });
-
-        try {
-            latch.await();
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-        }
-
-        return executionSessionList;
-	}
 }
