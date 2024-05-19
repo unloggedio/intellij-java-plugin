7475af13f3432106a6c0bf94545f25131a7d9c21
Kartikey Tewari <kt@unlogged.io>
2024-05-19 18:31:05 +0530

    add package discovery algorithm

--- src/main/java/com/insidious/plugin/factory/SessionLoader.java
+++ src/main/java/com/insidious/plugin/factory/SessionLoader.java
@@ -71,7 +71,59 @@ public class SessionLoader implements Runnable, GetProjectSessionsCallback, Disp
         }
     }
 
-    public void addSessionCallbackListener(GetProjectSessionsCallback getProjectSessionsCallback, SourceModel sourceModel, String packageName) {
+    private String getPackageName() {
+
+        String packageName = "";
+
+        List<VirtualFile> javaFiles = new ArrayList<>(ApplicationManager.getApplication()
+                .runReadAction((Computable<Collection<VirtualFile>>) () -> FileTypeIndex.
+                        getFiles(JavaFileType.INSTANCE, GlobalJavaSearchContext.projectScope(project))));
+
+        for (VirtualFile virtualFile: javaFiles) {
+            PsiFile psiFile = ApplicationManager.getApplication().runReadAction((Computable<PsiFile>) () ->
+                    PsiManager.getInstance(project).findFile(virtualFile));
+
+            if (psiFile instanceof PsiJavaFile) {
+                PsiJavaFile psiJavaFile = (PsiJavaFile) psiFile;
+                PsiClass[] javaFileClasses = ApplicationManager.getApplication()
+                        .runReadAction((Computable<PsiClass[]>) () -> psiJavaFile.getClasses());
+                for (PsiClass javaFileClass : javaFileClasses) {
+                    if (isMainClass(javaFileClass)) {
+                        String localPackage = ApplicationManager.getApplication()
+                                .runReadAction((Computable<String>) () -> psiJavaFile.getPackageName());
+                        packageName = localPackage;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return packageName;
+    }
+
+    private boolean isMainClass(PsiClass psiClass) {
+        if (psiClass == null || psiClass.getName() == null) {
+            return false;
+        }
+
+        return ApplicationManager.getApplication().runReadAction((Computable<Boolean>) () -> {
+            PsiMethod[] methods = psiClass.findMethodsByName("main", false);
+            for (PsiMethod method : methods) {
+                if (method.hasModifierProperty(PsiModifier.STATIC) && method.hasModifierProperty(PsiModifier.PUBLIC)) {
+                    PsiParameter[] parameters = method.getParameterList().getParameters();
+                    if (parameters.length == 1 && parameters[0].getType() instanceof PsiArrayType) {
+                        PsiType componentType = ((PsiArrayType) parameters[0].getType()).getComponentType();
+                        if (PsiType.getJavaLangString(method.getManager(), method.getResolveScope()).equals(componentType)) {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        });
+    }
+
+    public void addSessionCallbackListener(GetProjectSessionsCallback getProjectSessionsCallback) {
         this.listeners.add(getProjectSessionsCallback);
         if (lastResult != null) {
             getProjectSessionsCallback.success(lastResult);
