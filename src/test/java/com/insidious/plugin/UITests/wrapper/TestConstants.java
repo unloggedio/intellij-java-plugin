29235b904a39c0fac50b05ec2907f31e72f8ddf4
Kartikey Tewari <kt@unlogged.io>
2024-07-29 18:03:37 +0530

    set license key from env variable

--- src/test/java/com/insidious/plugin/UITests/wrapper/TestConstants.java
+++ src/test/java/com/insidious/plugin/UITests/wrapper/TestConstants.java
@@ -5,7 +5,7 @@ import static com.insidious.plugin.Constants.AGENT_VERSION;
 public class TestConstants {
     public static final String REMOTE_URL = "http://localhost:8123";
     public static final String DEFAULT_PRE_POPULATED_URL = "http://unlogged.local:8123";
-    public static final String VALID_LICENSE_KEY = "YOUR_VALID_KEY";
+    public static final String VALID_LICENSE_KEY = System.getenv("VALID_LICENSE_KEY_ENV");
     public static String MAVEN_DEPENDENCY_TEMPLATE = "<dependency>\n" +
             "  <artifactId>unlogged-sdk</artifactId>\n" +
             "  <groupId>video.bug</groupId>\n" +
